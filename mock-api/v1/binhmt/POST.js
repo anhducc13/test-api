086bd3e03e68f12665bf0332dd3cec4fb3d807f7
Tien Duc <duc.tt@teko.vn>
2022-10-18 13:00:30 +0700

    update

--- mock-api/v1/binhmt/POST.js
+++ mock-api/v1/binhmt/POST.js
@@ -2,11 +2,17 @@ module.exports = (request, response) => {
   const mappingQA = {
     answer1: 'A',
     answer2: 'B',
+    answer3: 'C',
+    answer4: 'da3',
+    answer5: 'sre',
+    answer6: 'cpm',
+    answer7: 'bi',
+    answer8: 'deprecated',
   }
   const body = request.body;
   const keys = Object.keys(body).filter(el => el.startsWith('answer'));
   if (keys?.[0]) {
-    if (body[keys[0]] === mappingQA[keys[0]]) {
+    if (body?.[keys[0]]?.toLowerCase?.().trim?.() === mappingQA?.[keys[0]]?.toLowerCase?.().trim?.()) {
       return response.status(200).json({
         message: 'Trả lời đúng',
         code: 1
